7fe06cc1701191d527d7f3ccb8fb7dfd93ab07bd
Aleph <aleph@covinette.net>
2023-02-09 02:03:57 +0300

    Начало работы над главной страницей

--- website/tour/views.py
+++ website/tour/views.py
@@ -1,4 +1,13 @@
 from django.views.generic import TemplateView
+from configparser import ConfigParser
+
+config = ConfigParser()
+config.read('secret.conf')
 
 class TourListView(TemplateView):
-    template_name = "tour-list.html"
+    template_name = "TourList.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['TourListApiKey'] = config.get('main', 'api_key')
+        return context
